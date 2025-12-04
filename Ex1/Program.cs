0ef8e88cfff6ced25e7d0009cc233696d64aad18
Ana Clara <acccarneiro@sga.pucminas.br>
2025-12-04 14:59:38 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NomeFelinos();
+        }
+        static void NomeFelinos()
+        {
+            try
+            {
+                StreamReader arq = new StreamReader("C:\\Users\\igorc\\source\\repos\\Lista 10 - ATP\\arquivo-01.txt");
+
+                string[] linhas = new string[10];
+                int quant = 0;
+                string linha;
+
+                linha = arq.ReadLine();
+
+                while (linha != null)
+                {
+                    linhas[quant] = linha;
+                    quant++;
+                    linha = arq.ReadLine();
+                }
+
+                arq.Close();
+
+                for (int i = 0; i < quant; i++)
+                {
+                    string[] partes = linhas[i].Split(';');
+                    Console.WriteLine(partes[1]); 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao ler arquivo: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*02. Escreva um método que imprima o maior peso, o menor peso e a média de peso dos animais presentes no arquivo.*/
+            MaiorPeso();
+        }
+        static void MaiorPeso()
+        {
+            try
+            {
+                StreamReader arq = new StreamReader("C:\\Users\\igorc\\source\\repos\\Lista 10 - ATP\\arquivo-01.txt");
+
+                string[] linhas = new string[10];
+                int quant = 0;
+                string linha;
+
+                linha = arq.ReadLine();
+
+                while (linha != null)
+                {
+                    linhas[quant] = linha;
+                    quant++;
+                    linha = arq.ReadLine();
+                }
+
+                arq.Close();
+
+                int maior = int.MinValue;
+                int menor = int.MaxValue;
+                int peso, soma = 0;
+                double media = 0;
+
+                for (int i = 0; i < quant; i++)
+                {
+                    string[] partes = linhas[i].Split(';');
+                    peso = int.Parse(partes[4]);
+
+                    if (peso > maior)
+                    {
+                        maior = peso;
+                    }
+                    if (peso < menor)
+                    {
+                        menor = peso;
+                    }
+
+                    soma += peso;
+                }
+
+                media = (double)soma / quant;
+
+                Console.WriteLine($"Menor peso: {menor}\n" +
+                    $"Maior peso: {maior}\n" +
+                    $"Média dos pesos: {media}");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro" + e.Message);
+            }
+        }
+    }
+}
